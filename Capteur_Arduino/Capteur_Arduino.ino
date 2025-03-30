6e8062b652e71e92019929a9d0b746bef1c7abe7
Yoann <laikouns@insa-toulouse.fr>
2025-03-30 14:20:42 +0200

    Building Menu (Rotary encoder and OLED screen)

--- Capteur_Arduino/Capteur_Arduino.ino
+++ Capteur_Arduino/Capteur_Arduino.ino
@@ -19,9 +19,9 @@ Last update: 17/03/2025
 //====================================================
 //==================== Libraries =====================
 //====================================================
-#include <Adafruit_SSD1306.h>         // Library of the OLED screen
-#include <Servo.h>                    // Library of the servo motor
-
+#include <Adafruit_SSD1306.h>  // Library of the OLED screen
+#include <Servo.h>             // Library of the servo motor
+#include <SPI.h>               // Library for SPI to control Digital Potentiometer
 
 
 //====================================================
@@ -31,30 +31,33 @@ Last update: 17/03/2025
 // OLED Screen
 #define nombreDePixelsEnLargeur 128
 #define nombreDePixelsEnHauteur 64
-#define brocheResetOLED         -1
-#define adresseI2CecranOLED     0x3C    // À définir
+#define brocheResetOLED -1
+#define adresseI2CecranOLED 0x3C  // À définir
 Adafruit_SSD1306 ecranOLED(nombreDePixelsEnLargeur, nombreDePixelsEnHauteur, &Wire, brocheResetOLED);
 // Rotary Encoder
-#define pinEncoder_CLK  2    // CLK output
-#define pinEncoder_DT 3      // DT Output
-#define pinEncoder_SW 4      // Switch connection 
-volatile int encoderPos = 0 ;
+#define pinEncoder_CLK 2  // CLK output
+#define pinEncoder_DT 3   // DT Output
+#define pinEncoder_SW 4   // Switch connection
+volatile int encoderPos = 0;
 // Graphite Sensor
-#define GraphiteSensor A0
+#define pinGraphiteSensor A0
 // Digital Potentiometer
-#define pinPot_CS 10
-#define pinPot_SCK 13
+#define pinPot_CS 10      //pin 10 to control Digital Potentiometer
+#define pinPot_SCK 13     //
 #define pinPot_SDI 11
+#define MCP_NOP 0b00000000
+#define MCP_WRITE 0b00010001
+#define MCP_SHTDWN 0b00100001
 // Flex Sensor
-const int flexPin = A2 ;
+const int flexPin = A2;
 // Bluetooth
 #define pinBT_TXD 5
 #define pinBT_RXD 6
-byte serialRX     // variable to receive data through RX
-byte serialTX     // variable to transfert data through TX
+byte serialRX;  // variable to receive data through RX
+byte serialTX;  // variable to transfert data through TX
 volatile byte RX = 0;
 // Motor
-#define pinnMoteur 9 ;
+#define pinnMoteur 9;
 
 
 
@@ -62,38 +65,36 @@ volatile byte RX = 0;
 //====================== Setup =======================
 //====================================================
 void setup() {
-  Serial.begin (9600);
+  Serial.begin(9600);
   Serial.println("----- Programme Capteur Start -----");
 
   // OLED Screen
-  SetOLED() ;
+  SetOLED();
 
   // Rotary Encoder
-  pinMode(pinEncoder_CLK, OUTPUT) ;
+  pinMode(pinEncoder_CLK, OUTPUT);
   digitalWrite(pinEncoder_CLK, HIGH);
-  pinMode(pinEncoder_DT, INPUT) ;
+  pinMode(pinEncoder_DT, INPUT);
   digitalWrite(pinEncoder_DT, HIGH);
-  pinMode(pinEncoder_SW, INPUT) ;
-  volatile int encoderPos = 0 ;
-  volatile int encoderPosBefore = 0 ;
-  attachInterrupt(0, doEncoder, RISING) ;
+  pinMode(pinEncoder_SW, INPUT);
+  volatile int encoderPos = 0;
+  volatile int encoderPosBefore = 0;
+  attachInterrupt(0, doEncoder, RISING);
 
   // Graphite Sensor
-  pinMode(,) ;
-  pinMode(,) ;
+  pinMode(pinGraphiteSensor, INPUT);
   // Digital Potentiometer
-  pinMode(,) ;
-  pinMode(,) ;
+  pinMode(pinPot_CS, OUTPUT);
+  digitalWrite(pinPot_CS, HIGH) ;   //SPI chip disabled
+  SPIWrite(MCP_WRITE, i, ssMCPin);                          // A DETERMINER !!!!!!
+    //pinMode(pinPot_SCK, );
+    //pinMode(pinPot_SDI, ) ;
   // Flex Sensor
-  pinMode(flexPin, INPUT) ;
+  pinMode(flexPin, INPUT);
   // Bluetooth
   setupBluetoothConnection();
   // Motor
-  My_Servo.attach(9);       // attaches the servo on pin 9 to the servo object   
-
-
-
-
+  My_Servo.attach(9);  // attaches the servo on pin 9 to the servo object
 }
 
 
@@ -106,17 +107,18 @@ void loop() {
   // refer to setup()
 
   // Mesure valeur en fonction du mode
-  DisplayOLED() ;
+  ecranOLED.print("Main Menu:");
+  ecranOLED.print(">> Fonction");
+  ecranOLED.print(">> Capteur")
+  ecranOLED.display();
 
 
   // Affiche menu en fonction du mode
-  
-  
+
+
 
 
   // Envoie les données avec le bluetooth
-  
-
 }
 
 
@@ -125,43 +127,51 @@ void loop() {
 //==================== Main Loop =====================
 //====================================================
 
-void doEncoder() {
-  if (digitalRead(pinEncoder_CLK)==HIGH && digitalRead(pinEncoder_DT)==HIGH) {
-    encoderPos-- ;
-  } 
-  else if (digitalRead(pinEncoder_CLK)==HIGH && digitalRead(pinEncoder_DT)==LOW) {
-    encoderPos++ ;
-  }
-}
 
-
+// Function for oled Screen
 void SetOLED() {
-  if(!ecranOLED.begin(SSD1306_SWITCHCAPVCC, adresseI2CecranOLED)){
+  if (!ecranOLED.begin(SSD1306_SWITCHCAPVCC, adresseI2CecranOLED)) {
     Serial.println("Initialisation OLED screen failed");
-    while(1);
+    while (1)
+      ;
   }
-  byte tailleDeCaractere = 1 ;
-  boolean bCouleurInverse = false ;
-  ecranOLED.clearDisplay();                   // Effacage de l'intensité du buffer
-  ecranOLED.setTextSize(tailleDeCaractere);   // Taille des caractères (1:1)
-  ecranOLED.setCursor(0,0);                   // Déplacement du curseur en position (0,0),dans l'angle supérieur gauche 
+  byte tailleDeCaractere = 1;
+  boolean bCouleurInverse = false;
+  ecranOLED.clearDisplay();                  // Effacage de l'intensité du buffer
+  ecranOLED.setTextSize(2);  // Taille des caractères (1:1)
+  ecranOLED.setCursor(0, 0);                 // Déplacement du curseur en position (0,0),dans l'angle supérieur gauche
 }
 
-void 
-
-void DisplayOLED(){
+void DisplayOLED() {
   if (encoderPos < encoderPos) {
-    ecranOLED.setCursor(10,0) ;
-    ecranOLED.print("Man Menu:") ;
-    ecranOLED.setCursor(0,9) ;
-    ecranOLED.print("> Fonction") ;
-    ecranOLED.print("> Capteur")
-    ecranOLED.display() ;
-  } 
-  else if (encoderPos > encoderPos) {
-
+    ecranOLED.print("Main Menu:");
+    ecranOLED.print(">> Fonction");
+    ecranOLED.print(">> Capteur")
+    ecranOLED.display();
+  } else if (encoderPos > encoderPos) {
   }
-  encoderPosBefore = encoderPos ;
-  
+  encoderPosBefore = encoderPos;
 }
 
+
+// Function for Digital potentiometer
+void doEncoder() {
+  if (digitalRead(pinEncoder_CLK) == HIGH && digitalRead(pinEncoder_DT) == HIGH) {
+    encoderPos--;
+  } else if (digitalRead(pinEncoder_CLK) == HIGH && digitalRead(pinEncoder_DT) == LOW) {
+    encoderPos++;
+  }
+}
+
+void SPIWrite(uint8_t cmd, uint8_t data, uint8_t ssPin) // SPI write the command and data to the MCP IC connected to the ssPin
+{
+  SPI.beginTransaction(SPISettings(14000000, MSBFIRST, SPI_MODE0)); //https://www.arduino.cc/en/Reference/SPISettings
+  
+  digitalWrite(ssPin, LOW); // SS pin low to select chip
+  
+  SPI.transfer(cmd);        // Send command code
+  SPI.transfer(data);       // Send associated value
+  
+  digitalWrite(ssPin, HIGH);// SS pin high to de-select chip
+  SPI.endTransaction();
+}
